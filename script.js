c2776e56b07b83675c53083f8f4afd953b6e0654
Micheal <mlowell94@gmail.com>
2022-06-04 21:07:24 -0400

    Adds functionality for all integer operations.

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const current = document.querySelector('.current');
 const numbers = document.querySelectorAll('.numbers > div');
 const operators = document.querySelectorAll('.operators > div')
 
-let currentNumbers = [];
+let currentFirst = null;
+let currentSecond = null;
 let currentOperator = '';
 let currentStringInt = '';
+let decPressed = false;
 
 numbers.forEach(number => {
     number.addEventListener('click', function(e) {
@@ -58,11 +60,153 @@ numbers.forEach(number => {
                 currentStringInt += '9';
                 current.textContent = currentStringInt;
                 break;
+            case '.': 
+                if(!decPressed) {
+                    currentStringInt += '.';
+                    current.textContent = currentStringInt;
+                    decPressed = true;
+                } else {
+                    break;
+                }
             case '=':
-                execute(currentNumbers, currentOperator);
+                operators.forEach(operator => operator.removeAttribute('id'));
+                if(currentFirst == null) {
+                    break;
+                }
+                if(currentStringInt == '') {
+                    previous.textContent = `${currentFirst} ${currentOperator} ${currentSecond}`
+                    result = operate(currentFirst,currentSecond,currentOperator);
+                    currentFirst = result;
+                    current.textContent = result;
+                    console.log(currentFirst);
+                    console.log(currentSecond);
+                    break;
+                } else {
+                    if(currentStringInt.includes('.')) {
+                        currentSecond = parseFloat(currentStringInt);
+                    } else {
+                        currentSecond = parseInt(currentStringInt);
+                    }
+                currentStringInt = '';
+                previous.textContent = `${currentFirst} ${currentOperator} ${currentSecond}`
+                result = operate(currentFirst,currentSecond,currentOperator);
+                currentFirst = result;
+                current.textContent = result;
+                console.log(currentFirst);
+                console.log(currentSecond);
+                break;
+                }
+            default:
+                console.log('Error');
+        }
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', function(e) {
+        operators.forEach(operator => operator.removeAttribute('id'));
+        operator.setAttribute('id','press');
+        switch(operator.textContent) {
+            case '+':
+                if(currentFirst == null) {
+                    currentFirst = parseInt(currentStringInt);
+                    currentStringInt = '';
+                } else {
+                    if(currentStringInt == '') {
+                        previous.textContent = current.textContent;
+                        current.textContent = '';
+                    } else {
+                    currentSecond = parseInt(currentStringInt);
+                    currentStringInt = '';
+                    previous.textContent = `${currentFirst} ${currentOperator} ${currentSecond}`
+                    result = operate(currentFirst,currentSecond,currentOperator);
+                    currentFirst = result;
+                    current.textContent = result;    
+                    }
+                }
+                currentOperator = '+';
+                break;
+            case '-':
+                if(currentFirst == null) {
+                    currentFirst = parseInt(currentStringInt);
+                    currentStringInt = '';
+                } else {
+                    if(currentStringInt == '') {
+                        previous.textContent = current.textContent;
+                        current.textContent = '';
+                    } else {
+                    currentSecond = parseInt(currentStringInt);
+                    currentStringInt = '';
+                    previous.textContent = `${currentFirst} ${currentOperator} ${currentSecond}`
+                    result = operate(currentFirst,currentSecond,currentOperator);
+                    currentFirst = result;
+                    current.textContent = result;    
+                    }
+                }
+                currentOperator = '-';
+                break;
+            case '%':
+                if(currentFirst == null) {
+                    currentFirst = parseInt(currentStringInt);
+                    currentStringInt = '';
+                } else {
+                    if(currentStringInt == '') {
+                        previous.textContent = current.textContent;
+                        current.textContent = '';
+                    } else {
+                    currentSecond = parseInt(currentStringInt);
+                    currentStringInt = '';
+                    previous.textContent = `${currentFirst} ${currentOperator} ${currentSecond}`
+                    result = operate(currentFirst,currentSecond,currentOperator);
+                    currentFirst = result;
+                    current.textContent = result;    
+                    }
+                }
+                currentOperator = '%';
+                break;
+            case 'x':
+                if(currentFirst == null) {
+                    currentFirst = parseInt(currentStringInt);
+                    currentStringInt = '';
+                } else {
+                    if(currentStringInt == '') {
+                        previous.textContent = current.textContent;
+                        current.textContent = '';
+                    } else {
+                    currentSecond = parseInt(currentStringInt);
+                    currentStringInt = '';
+                    previous.textContent = `${currentFirst} ${currentOperator} ${currentSecond}`
+                    result = operate(currentFirst,currentSecond,currentOperator);
+                    currentFirst = result;
+                    current.textContent = result;    
+                    }
+                }
+                currentOperator = 'x';
                 break;
             default:
                 console.log('Error');
         }
     });
 });
+
+function operate(first,second,ope) {
+    result = 0;
+    switch(ope) {
+        case '+':
+            result = first + second;
+            break;
+        case '-':
+            result = first - second;
+            break;
+        case '%':
+            result = first / second;
+            break;
+        case 'x':
+            result = first * second;
+            break;
+        default:
+            console.log(error);
+    }
+    return result;
+}
+
